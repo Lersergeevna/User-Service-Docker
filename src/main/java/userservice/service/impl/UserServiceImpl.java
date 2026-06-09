cd636816f982bfa8e578962c49c017e8b8a0d428
Lersergeevna <sergeevna.leva@gmail.com>
2026-06-09 15:17:45 +0300

    Change UserServiceImpl

--- src/main/java/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/userservice/service/impl/UserServiceImpl.java
@@ -21,8 +21,8 @@ import java.util.List;
  */
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
+
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
